1147650f7cb013fa49740ecd3c4519eeab52b8c4
Andini Anissa <andinianst93@Andinis-Laptop.local>
2022-09-02 15:03:37 +0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non mollitia
+        libero id molestias sit, repellat deleniti enim magni eaque laborum
+        fugiat? Placeat, tempora inventore vero, consequatur iure possimus
+        libero ut illo voluptatum, ipsum velit quod. Voluptas cum nesciunt
+        repudiandae minus? Placeat provident repellendus iure ipsum eligendi
+        perspiciatis quo numquam adipisci.
+      </p>
+    </section>
+  )
+}
+
+export default About
