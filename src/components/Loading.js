91c010ae1cc60f97f2be165fa0ac3be3103dc9cf
andinianst93 <andinianst93@gmail.com>
2022-09-02 17:04:27 +0700

    loading fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Loading = () => {
-  return <h2 className='loading'>Loading....</h2>
+  return <h2 className='loading'>Loading...</h2>
 }
 
 export default Loading
